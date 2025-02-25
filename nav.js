2f4d066d7ecb22334c2751198593b8a22b5a9be5
Biel Costa <118078513+BielC19@users.noreply.github.com>
2025-02-25 18:18:11 +0100

    tot

---
+++ nav.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navMenu = `
+    <ul>
+    <li><a id="index" class="primer" href="index.html">Home</a></li>
+    <li><a id="m_leter" class="segon" href="m_leter.html">Sobre mi</a></li>
+    <li><a id="docs" class="tercer" href="docs.html">Documents</a></li>
+    </ul>
+    `
+    const navContainer = document.getElementById("barr")
+    
+    if (navContainer) {
+        navContainer.innerHTML = navMenu
+    } else {
+        console.error("Navigation container not found")
+    }
+    
+    const url = window.location.pathname.split('/');
+    console.log(url)
+    const keys = ["index.html", "m_leter.html", "docs.html", "logo.html"]
+    const keys2 = ["index", "m_leter", "docs", "logo"]
+    const celsNu = keys.length
+    let rr = 0
+    for (let i = 0; i < celsNu; i++) {
+    console.log("rr")
+        if (url[1] === keys[i]) {
+            rr = i
+            console.log(rr)
+        }
+    console.log(rr)
+    }
+    document.getElementById(keys2[rr]).classList.add("color")
+
+})    
+
